9af2d32334516a7a37a9f8ef46d26fbf7a85d2c3
misha <misha.kotliar@gmail.com>
2016-07-10 23:48:45 +0300

    First commit. Linked libraries

---
+++ main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include "include/api/BamReader.h"
+#include "include/api/BamMultiReader.h"
+
+using namespace std;
+using namespace BamTools;
+
+int main() {
+    cout << "Hello, World!" << endl;
+
+    BamReader reader;
+
+    return 0;
+}
