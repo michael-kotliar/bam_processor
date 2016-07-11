f43d3777f540949bbbb9a4f41a2046cfea31bc0f
misha <misha.kotliar@gmail.com>
2016-07-11 05:38:20 +0300

    loads bam file

--- main.cpp
+++ main.cpp
@@ -5,10 +5,38 @@
 using namespace std;
 using namespace BamTools;
 
+
+
+
+
 int main() {
-    cout << "Hello, World!" << endl;
+
+//    cout << "Set full path to bam file" << endl;
+    string full_filename;
+//    cin >> full_filename;
+    full_filename = "/home/jedi/cchmc/data/bam/small/wgEncodeUwRepliSeqBjG1bAlnRep1.bam";
 
     BamReader reader;
+    if (not reader.Open(full_filename)) {
+        cout << "Couldn't open file " << full_filename << endl;
+        return 0;
+    } else {
+        cout << "Open " << reader.GetFilename() << endl;
+    };
+
+    SamHeader header = reader.GetHeader();
+    cout << "Header " << "[" << reader.GetErrorString() << "]" << endl;
+    cout << "ReferenceCount " << "["<< reader.GetReferenceCount() << "]" << endl;
+    RefVector ref_data_vector = reader.GetReferenceData();
+
+    for (int i = 0; i < ref_data_vector.size(); i++ ){
+        RefData current_ref_data = ref_data_vector[i];
+        cout << i << endl;
+        cout << "  RefName " << current_ref_data.RefName << endl;
+        cout << "  RefLenght " << current_ref_data.RefLength << endl;
+    }
+
+
 
     return 0;
 }
